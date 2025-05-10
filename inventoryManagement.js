f8b1049525b966ddfea8bc8c25fd325999ba41d8
Ridge <ridge.telegrafi@icloud.com>
2025-05-10 03:05:41 -0400

    Implemented add, update, remove, and log helpers

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,20 @@
 // Write your code here
+let products =["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {return console.log (products[0]);
+}
+
+//Adds an item at the end of the array
+function addProduct(name){products.push(name);}
+
+//checks that the new position is valid (number should be positive, and not grater than the lengsth od the array)
+function updateProductName(positionInArray, newName){if (positionInArray>=0 && positionInArray<products.length)
+                                                      products[positionInArray]= newName;
+                                                      else {console.log("Please enter a valid position to add the new product in.")}
+}
+
+
+function removeLastProduct() { products.pop();}
 
 
 // Export the necessary parts for testing
